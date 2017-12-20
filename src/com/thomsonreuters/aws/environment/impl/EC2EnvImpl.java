b0d2e2d357ebf23d33ebd032c278e3408d09aac5
Dean <Andrew.Dean@thomsonreuters.com>
2017-12-20 09:04:47 +0000

    removed reservations layer for instances

--- src/com/thomsonreuters/aws/environment/impl/EC2EnvImpl.java
+++ src/com/thomsonreuters/aws/environment/impl/EC2EnvImpl.java
@@ -5,8 +5,13 @@
  */
 package com.thomsonreuters.aws.environment.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.thomsonreuters.aws.ami.IAmis;
 import com.thomsonreuters.aws.ami.impl.AmisImpl;
 import com.thomsonreuters.aws.ec2.IEC2s;
@@ -39,7 +44,12 @@ public class EC2EnvImpl implements IEC2Env {
 	    @Override
 	    public IEC2s describeEC2s(IDescribeEC2sRequest request) {
 	        IDescribeEC2sRequestRaw raw = (IDescribeEC2sRequestRaw)request;
-	        return new EC2sImpl(_ec2.describeInstances(raw.getRaw()).getReservations().get(0).getInstances());
+	        List<Reservation> resList = _ec2.describeInstances(raw.getRaw()).getReservations();
+	        List<Instance> instances = new ArrayList<>();
+	        for (Reservation res : resList) {
+	        	instances.addAll(res.getInstances());
+	        }
+	        return new EC2sImpl(instances);
 	    }
 }
 
