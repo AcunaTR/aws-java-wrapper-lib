403a6fecf06d1cae78c13a303eae80a40c328ead
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-09 11:05:41 +0000

    Project restructure for maven

---
+++ Acuna-Library/src/main/java/com/thomsonreuters/aws/environment/ec2/request/impl/DescribeEC2sRequestImpl.java
@@ -0,0 +1,47 @@
+package com.thomsonreuters.aws.environment.ec2.request.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.Filter;
+
+import com.thomsonreuters.aws.environment.ec2.request.IDescribeEC2sRequest;
+import com.thomsonreuters.aws.filter.IFilter;
+import com.thomsonreuters.aws.filter.IFilters;
+import com.thomsonreuters.aws.filter.impl.IFilterRaw;
+import com.thomsonreuters.aws.filter.impl.IFiltersRaw;
+
+public class DescribeEC2sRequestImpl implements IDescribeEC2sRequest, IDescribeEC2sRequestRaw{
+
+    private final DescribeInstancesRequest _req;
+    private List<Filter> _filters;
+	
+    public DescribeEC2sRequestImpl() {
+        _req = new DescribeInstancesRequest();
+        _filters = new ArrayList<>();
+    }
+
+    @Override
+    public DescribeInstancesRequest getRaw() {
+        _req.setFilters(_filters);
+        return _req;
+    }
+
+    @Override
+    public void addFilter(IFilter filter) {
+        IFilterRaw raw = (IFilterRaw) filter;
+        _filters.add(raw.getRaw());
+    }
+
+    @Override
+    public void addFilters(IFilters filters) {
+        IFiltersRaw raw = (IFiltersRaw) filters;
+        _filters.addAll(raw.getRaw());
+    }
+    
+    @Override
+	public String toString() {
+		return _req.toString() + _filters.toString();
+	}
+}
---
+++ Acuna-Library/src/main/java/com/thomsonreuters/aws/filter/impl/FiltersImpl.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thomsonreuters.aws.filter.impl;
+
+import com.amazonaws.services.ec2.model.Filter;
+
+import com.thomsonreuters.aws.filter.IFilter;
+import com.thomsonreuters.aws.filter.IFilters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author U6067157
+ */
+public class FiltersImpl implements IFilters, IFiltersRaw {
+
+    private final List<Filter> _filters;
+    
+    public FiltersImpl() {
+        _filters = new ArrayList<>();
+    }
+    
+    public FiltersImpl(List<IFilter> filters) {
+    	_filters = new ArrayList<>();
+    	filters.forEach(filter -> {addFilter(filter);});
+	}
+
+    @Override
+    public void addFilter(IFilter filter) {
+        IFilterRaw raw = (IFilterRaw)filter;
+        _filters.add(raw.getRaw());
+    }
+
+    @Override
+    public List<Filter> getRaw() {
+        return _filters;
+    }
+
+    @Override
+    public IFilter getFilter(int idx) {
+        return new FilterImpl(_filters.get(idx));
+    }
+
+    @Override
+    public void addAllFilters(IFilters filters) {
+        IFiltersRaw raw = (IFiltersRaw) filters;
+        raw.getRaw().forEach(filter -> {_filters.add(filter);});
+    }
+
+    @Override
+    public List<IFilter> getFilters() {
+        List<IFilter> filters = new ArrayList<>();
+        _filters.forEach(filter -> filters.add(new FilterImpl(filter)));
+        return filters;
+    }
+    
+    @Override
+	public String toString() {
+		return _filters.toString();
+	}
+}
