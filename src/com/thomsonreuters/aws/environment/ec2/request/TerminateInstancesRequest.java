fbee8f7aa736c371575dd546d48fb4c11eb95dd6
Pressman <Annette.Pressman@thomsonreuters.com>
2018-01-10 09:26:27 +0000

    blank space

--- src/com/thomsonreuters/aws/environment/ec2/request/TerminateInstancesRequest.java
+++ src/com/thomsonreuters/aws/environment/ec2/request/TerminateInstancesRequest.java
@@ -1,6 +1,5 @@
 package com.thomsonreuters.aws.environment.ec2.request;
 
-import com.thomsonreuters.aws.environment.ec2.request.impl.DescribeEC2sRequestImpl;
 import com.thomsonreuters.aws.environment.ec2.request.impl.TerminateInstancesRequestImpl;
 
 public class TerminateInstancesRequest {
