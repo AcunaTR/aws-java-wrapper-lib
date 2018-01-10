a8dae1f453cd51596e76614e8581971d38f0a40c
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-10 10:31:19 +0000

    Added dynamic versioning

--- Pipeline/deploy.sh
+++ Pipeline/deploy.sh
@@ -10,6 +10,7 @@ VERSION=$(cat ./version)
 BUILD_VERSION=$VERSION.$BUILD_NUMBER
 
 docker run --rm \
+		   --env BUILD_NUMBER=BUILD_NUMBER \
            -v "${CURDIR}/:/build" \
            --workdir /build/Acuna-Library \
            maven:3.5.2-jdk-8 mvn deploy
