94fb7e067f045244d2fde21c893768946995ac63
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-10 10:37:25 +0000

    Added dynamic versioning

--- Pipeline/package.sh
+++ Pipeline/package.sh
@@ -13,4 +13,4 @@ docker run --rm \
 		   --env BUILD_NUMBER=$BUILD_NUMBER \
            -v "${CURDIR}/:/build" \
            --workdir /build/Acuna-Library \
-           maven:3.5.2-jdk-8 mvn package
+           maven:3.5.2-jdk-8 echo "In container" && echo $BUILD_NUMBER
